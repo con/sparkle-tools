6a8b0d274caa9c32c7f0ae0c7e345612f01e5eff
Yaroslav Halchenko <debian@onerussian.com>
2021-06-23 10:05:00 -0400

    Initial full round of stats running in headless mode
    	$> ( source ../../sparkle-tools.secrets; python make_webshots.py https://ohbm.sparkle.space )
    	remotes/origin/env/ohbm
    	{
    	  "initial-login": {
    		"times": {
    		  "login": 5.0555877685546875,
    		  "main-screen-appear": 4.320533037185669,
    		  "participants-appear": 12.823843240737915
    		},
    		"sparkle-build-sha1": "80da6a05b4c5fa791e7e06d1a444449c0e01aec8",
    		"participants": 817,
    		"sparkle-build-describe": null
    	  },
    	  "total": 23.51283836364746
    	}
    
    	$> ( source ../../sparkle-tools.secrets; python make_webshots.py http://localhost:3000 )
    	{
    	  "initial-login": {
    		"times": {
    		  "login": 5.261866331100464,
    		  "main-screen-appear": 1.5135037899017334,
    		  "participants-appear": 2.2340309619903564
    		},
    		"sparkle-build-sha1": "",
    		"participants": 849
    	  },
    	  "total": 10.302043676376343
    	}

--- tools/make_webshots.py
+++ tools/make_webshots.py
@@ -20,6 +20,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import WebDriverException
 import yaml
+import subprocess
 import json
 
 log = logging.getLogger(__name__)
@@ -103,7 +104,7 @@ def login(driver, url, username, password):
     # from now on even finding an element becomes a "heavy task"
     # get info on what state we use
     e = driver.find_element_by_xpath('/html/head/meta[@name="sparkle-build-sha1"]')
-    rec['sparkle-build-sha1'] = e.get_attribute('content')
+    sha = rec['sparkle-build-sha1'] = e.get_attribute('content')
 
     # silly way to wait until we see some reasonable number of attendees
     for i in range(100):
@@ -120,112 +121,19 @@ def login(driver, url, username, password):
     else:
         raise RuntimeError("Did not get reasonable number of participants.")
     ts['participants-appear'] = timer()
+    rec['participants'] = n
+
+    if sha and os.environ.get("SPARKLE_CODEBASE"):
+        r = subprocess.run(
+            ['git', '-C', os.environ["SPARKLE_CODEBASE"], 'describe', '--all', sha]
+        )
+        if not r.returncode:  # only if didn't fail and found it
+            rec['sparkle-build-describe'] = r.stdout
     return rec
-
-
-def wait_no_progressbar(driver, cls):
-    WebDriverWait(driver, 300, poll_frequency=0.1).until(
-        EC.invisibility_of_element_located((By.CLASS_NAME, cls)))
 
 
 def wait_until(driver, until):
     return WebDriverWait(driver, 300, poll_frequency=0.1).until(until)
-
-
-def __process_dandiset(driver, ds):
-
-    def click_edit():
-        # might still take a bit to appear
-        # TODO: more sensible way to "identify" it: https://github.com/dandi/dandiarchive/issues/648
-        edit_button = WebDriverWait(driver, 3).until(
-            EC.element_to_be_clickable(
-                (By.XPATH,
-                 '//button[@id="view-edit-metadata"]'
-                 )))
-        edit_button.click()
-
-    dspath = Path(ds)
-    if not dspath.exists():
-        dspath.mkdir(parents=True)
-
-    info = {'times': {}}
-    times = info['times']
-    stats = []
-
-    # TODO: do not do draft unless there is one
-    # TODO: do for a released version
-    for urlsuf, page, wait, act in [
-        ('', 'landing', partial(wait_no_progressbar, driver, "v-progress-circular"), None),
-        (None, 'edit-metadata', partial(wait_no_progressbar, driver, "v-progress-circular"), click_edit),
-        ('/draft/files', 'view-data', partial(wait_no_progressbar, driver, "v-progress-linear"), None)]:
-
-        log.info(f"{ds} {page}")
-        page_name = dspath / page
-        # So we could try a few times in case of catching WebDriverException
-        # e.g. as in the case of "invalid session id" whenever we would reinitialize
-        # the entire driver
-        for trial in range(3):
-            page_name.with_suffix('.png').unlink(missing_ok=True)
-            t0 = time.monotonic()
-            # ad-hoc workaround for https://github.com/dandi/dandiarchive/issues/662
-            # with hope it is the only one and to not overcomplicate things
-            # so if we fail, we do not carry outdated one
-            #if ds in ('000040', '000041') and page == 'edit-metadata':
-            #    t = "timeout/crash"
-            #    break
-            try:
-                if urlsuf is not None:
-                    log.debug("Before get")
-                    driver.get(f'{ARCHIVE_GUI}/#/dandiset/{ds}{urlsuf}')
-                    log.debug("After get")
-                if act:
-                    log.debug("Before act")
-                    act()
-                    log.debug("After act")
-                if wait:
-                    log.debug("Before wait")
-                    wait()
-                    log.debug("After wait")
-            except TimeoutException:
-                log.debug("Timed out")
-                t = 'timeout'
-                break
-            except WebDriverException as exc:
-                # do not bother trying to resurrect - it seems to not working really based on
-                # 000040 timeout experience
-                raise
-                t = str(exc).rstrip()  # so even if we continue out of the loop
-                log.warning(f"Caught {exc}. Reinitializing")
-                # it might be a reason for subsequent "Max retries exceeded"
-                # since it closes "too much"
-                #try:
-                #    driver.quit()  # cleanup if still can
-                #finally:
-                driver = get_ready_driver()
-                continue
-            except Exception as exc:
-                log.warning(f"Caught unexpected {exc}.")
-                t = str(exc).rstrip()
-                break
-            else:
-                t = time.monotonic() - t0
-                time.sleep(2)  # to overcome https://github.com/dandi/dandiarchive/issues/650 - animations etc
-                driver.save_screenshot(str(page_name.with_suffix('.png')))
-                break
-        times[page] = t
-        stats.append(LoadStat(
-            dandiset=ds,
-            page=page,
-            time=t,
-            label='Edit Metadata' if page == 'edit-metadata' else 'Go to page',
-            url=f'{ARCHIVE_GUI}/#/dandiset/{ds}{urlsuf}' if urlsuf is not None else None,
-        ))
-        # now that we do login, do not bother storing html to not leak anything sensitive by mistake
-        # page_name.with_suffix('.html').write_text(driver.page_source)
-
-    with (dspath / 'info.yaml').open('w') as f:
-        yaml.safe_dump(info, f)
-    return stats
 
 
 # to help with "invalid session id" by reinitializing the entire driver
@@ -235,7 +143,7 @@ def get_ready_driver():
         options.add_argument('--new-window')
     else:
         options.add_argument('--no-sandbox')
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--incognito')
         # options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
@@ -255,16 +163,22 @@ if __name__ == '__main__':
     )
 
     if len(sys.argv) > 1:
-        url = sys.argv[1:]
+        url = sys.argv[1]
     else:
-        url = 'https://ohbm.sparkle.space/'
+        url = 'https://ohbm.sparkle.space'
+    url = url.rstrip('/')  # for consistency
 
     # To guarantee that we time out if something gets stuck
     socket.setdefaulttimeout(300)
+    overall = Timer()
     driver = get_ready_driver()
-    allstats = []
-    # yoh recommends to create a file with those secrets exported outside of the repo
-    rec = login(driver, url, os.environ["SPARKLE_USERNAME"], os.environ["SPARKLE_PASSWORD"])
-    print(json.dumps(rec, indent=2))
-    driver.quit()
+    try:
+        allstats = {}
+        # yoh recommends to create a file with those secrets exported outside of the repo
+        allstats['initial-login'] = login(driver, url, os.environ["SPARKLE_USERNAME"], os.environ["SPARKLE_PASSWORD"])
+
+        allstats['total'] = overall()
+        print(json.dumps(allstats, indent=2))
+    finally:
+        driver.quit()
 
